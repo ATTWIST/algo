7a0e06eb0e8e57fcf53145c8d0ad5e8371bc327e
ATTWIST <134682709+ATTWIST@users.noreply.github.com>
2023-05-26 17:15:09 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int A;
+int B;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> A;
+    cin >> B;
+    cout << A*B << '\n';
+    return 0;
+}
