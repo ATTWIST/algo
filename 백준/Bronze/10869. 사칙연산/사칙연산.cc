f0948c179310171da6574382a57b07b93ebf5ee4
ATTWIST <134682709+ATTWIST@users.noreply.github.com>
2023-05-26 17:15:13 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int A;
+int B;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> A >> B;
+    cout << A+B << '\n';
+    cout << A-B << '\n';
+    cout << A*B << '\n';
+    cout << A/B << '\n';
+    cout << A%B << '\n';
+    return 0;
+}
