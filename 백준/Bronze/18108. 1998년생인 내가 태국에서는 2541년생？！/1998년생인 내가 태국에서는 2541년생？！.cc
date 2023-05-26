a14109d1496841bf48eb31f2355c51c7dd7efc98
ATTWIST <134682709+ATTWIST@users.noreply.github.com>
2023-05-26 17:15:32 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int a;
+    cin >> a;
+    a -= 543;
+    cout << a << endl;
+    return 0;
+}
