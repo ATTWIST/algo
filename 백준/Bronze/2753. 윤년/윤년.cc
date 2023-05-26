acc4e4537b163de41f0e0119507dbc60480bd477
ATTWIST <134682709+ATTWIST@users.noreply.github.com>
2023-05-26 17:17:16 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a;
+
+int main(){
+    cin >> a;
+    if(a % 4 == 0 && a % 100 != 0 || a % 400 == 0  )
+        cout << "1" << '\n';
+    else cout << "0" << '\n';
+    
+}
