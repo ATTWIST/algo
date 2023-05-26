addb1b6e346b7a7040254ceb87f656a4e57cde2e
ATTWIST <134682709+ATTWIST@users.noreply.github.com>
2023-05-26 17:14:58 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+double A;
+double B;
+
+int main(){
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> A;
+    cin >> B;
+    cout.precision(10);
+    cout << A/B << '\n';
+    return 0;
+}
