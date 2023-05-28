54ebd19205f41acf4827591ef80c60812b69c18d
ATTWIST <134682709+ATTWIST@users.noreply.github.com>
2023-05-28 20:05:04 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;;
+int a,b,T;
+
+int main(){
+    cin >> T;
+    string bufferflush;
+    getline(cin,bufferflush);
+    for(int i = 0; i < T; i++){
+        cin >> a >> b;
+        cout << a+b << '\n';
+    }
+        
+}
