5a1b9ed27a68e9d7bf961d412817dda8ba44dead
ATTWIST <134682709+ATTWIST@users.noreply.github.com>
2023-05-29 14:56:28 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll a,b,T;
+string sum;
+
+int main(){
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> T;
+	for (int i = 1; i <= T; i++){
+		sum += "*";
+		cout << sum << '\n';
+	}
+	
+}
