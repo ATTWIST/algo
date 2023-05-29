79329ce77b6676ccb15c37f97573e45528b48e74
ATTWIST <134682709+ATTWIST@users.noreply.github.com>
2023-05-29 14:43:20 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll a,b,T;
+
+int main(){
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> T;
+	for (int i = 1; i <= T; i++){
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a+b << '\n';
+	}
+	
+}
