020f11898e259787c7253d0e4ae3d6ad9a158225
ATTWIST <134682709+ATTWIST@users.noreply.github.com>
2023-06-01 16:02:27 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int T, R;
+	string S;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> R >> S;
+		for (int j = 0; j < S.size(); j++) {
+			for (int k = 0; k < R; k++) {
+				cout << S[j];
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
