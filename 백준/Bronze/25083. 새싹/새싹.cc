7821ee70f9e19edc4043029e8d03da7de74b776d
ATTWIST <134682709+ATTWIST@users.noreply.github.com>
2023-06-02 13:53:54 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s0{"         ,r'\"7"};
+string s1{"r`-_   ,'  ,/"};
+string s2{" \\. \". L_r'"};
+string s3{"   `~\\/"};
+string s4{"      |"};
+string s5{"      |"};
+int main(){
+	cout << s0 << '\n';
+	cout << s1 << '\n';
+	cout << s2 << '\n';
+	cout << s3 << '\n';
+	cout << s4 << '\n';
+	cout << s5 << '\n';
+	
+}
