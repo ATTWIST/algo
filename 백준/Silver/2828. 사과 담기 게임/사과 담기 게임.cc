37a2f6ebc476ea1b4dbab11cdbb9aa9b106f05dd
ATTWIST <134682709+ATTWIST@users.noreply.github.com>
2023-09-22 10:20:38 +0900

    [Silver V] Title: 사과 담기 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
@@ -1,30 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int n,m,j,ap,l,r,bx;
+int n,m,j,bx,r,l,ret,ap;
 
 int main(){
-	cin >> n >> m;
-	if(n >= m){
-		cin >> j;
-		l = 1;
-		for(int i = 0; i < j; i++){
-			r = l + m -1;
-			cin >> ap;
-			if(ap >= l && ap <= r)continue;
-			
-			else{
-			 if(l < ap){
-			 	l += (ap - r);
-			 	bx += (ap - r);	
-			 }
-			 else {
-			 	bx += (l - ap);
-			 	l = ap;
-			 }
+	cin >> n >> m >> j;
+	l = 1;
+	for(int i = 0; i < j; i++){
+		cin >> ap;
+		r = l + m -1;
+		if(l <= ap && r >= ap)continue;
+		
+		else{
+		if(l < ap){
+			bx += ap - r;
+			l += ap - r;
 		}
+		else{
+		bx += l - ap;
+		l = ap;
+	}
+	}
 	}
 	cout << bx;
 }
-}
-    
